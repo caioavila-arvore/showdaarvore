c7a9da3cf6d96e5a4bdd87d15739aad48aa1d0f3
caioavila_arvore <caio.avila@arvoredelivros.com.br>
2016-05-10 14:54:59 -0300

    script quiz

---
+++ js/main.js
@@ -0,0 +1,144 @@
+var current_question
+var questions = [
+  {
+    title: "Quem descobriu o Brasil?",
+    alternatives: [
+      { text: "Resposta Errada 1", type: false},
+      { text: "Resposta Errada 2", type: false},
+      { text: "Resposta Errada 3", type: false},
+      { text: "Pedro Alvares Cabral", type: true }
+    ],
+    id: 1
+  },
+  {
+    title: "Outra Pergunta",
+    alternatives: [
+      { text: "Resposta Errada 1", type: false},
+      { text: "Resposta Errada 2", type: false},
+      { text: "Pedro Alvares Cabral", type: true },
+      { text: "Resposta Errada 3", type: false}
+    ],
+    id: 2
+  },
+  {
+    title: "Mais uma pergunta",
+    alternatives: [
+      { text: "Resposta Errada 1", type: false},
+      { text: "Pedro Alvares Cabral", type: true },
+      { text: "Resposta Errada 2", type: false},
+      { text: "Resposta Errada 3", type: false}
+    ],
+    id: 3
+  }
+]
+
+function display_question(question) {
+  change_elements_text(question)
+  create_events_for_question(question)
+}
+
+function change_elements_text(question) {
+  $("#title").text(question.title)
+  var alternatives = question.alternatives
+  for(var i=0; i < alternatives.length; i++) {
+    $("#alternative_" + i + " em").text(alternatives[i].text)
+  }
+}
+
+function create_events_for_question(question) {
+  var alternatives = question.alternatives
+  for(var i=0; i < alternatives.length; i++) {
+    var alternative = alternatives[i]
+    if(alternative.correct == true) {
+      $("#alternative_" + i).on("click", correct)
+    } else {
+      $("#alternative_" + i).on("click", wrong)
+    }
+  }
+}
+
+function correct(e) {
+  // effects
+  next_question();
+}
+
+function wrong(e) {
+  // effect
+  next_question();
+}
+
+function next_question(old_question) {
+  var id = old_question.id + 1
+  if (questions[id].length > 0) {
+    current_question = questions[id]
+  } else {
+    current_question = questions[0]
+  }
+  return current_question
+}
+
+
+function key_press(){
+  $(document).on("keypress", function(e){
+
+    if(e.keyCode == 49){
+      // 1
+      $(".options li").removeClass("selected");
+      $("#alternative_0").addClass("selected");
+    }
+
+    if(e.keyCode == 50){
+      // 2
+      $(".options li").removeClass("selected");
+      $("#alternative_1").addClass("selected");
+    }
+
+    if(e.keyCode == 51){
+      // 3
+      $(".options li").removeClass("selected");
+      $("#alternative_2").addClass("selected");
+    }
+
+    if(e.keyCode == 52){
+      // 4
+      $(".options li").removeClass("selected");
+      $("#alternative_3").addClass("selected");
+    }
+
+    if(e.keyCode == 13){
+      // saber resposta
+      var alternatives = current_question.alternatives;
+      for(var i=0; i < alternatives.length; i++) {
+        if (alternatives[i].type == true) {
+          $(".options li").removeClass("correct");
+          $("#alternative_" + i).addClass("correct");
+        }
+      }
+    }
+
+    if(e.keyCode == 110){
+      // n
+
+      $(".options li").removeClass("correct");
+      $(".options li").removeClass("selected");
+      start()
+    }
+
+  });
+}
+
+
+function start() {
+  var id_question = Math.floor( Math.random() * (questions.length))
+  current_question = questions[id_question]
+  console.log(current_question)
+  display_question(current_question)
+}
+
+
+$(document).ready(function(){
+  start()
+
+  key_press();
+
+});
